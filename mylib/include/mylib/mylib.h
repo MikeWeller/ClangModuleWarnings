3209331d4de3d7cc0c9dead5d86d5b9216055c26
Mike Weller <mike@mikeweller.com>
2016-08-11 09:47:14 +0100

    [minor] change to mylib.h comments

--- mylib/include/mylib/mylib.h
+++ mylib/include/mylib/mylib.h
@@ -7,8 +7,8 @@ template <typename TFloat>
 bool exactlyEqual(TFloat lhs, TFloat rhs)
 {
     // Pragma required for internal code using this header
-    // External clients won't see the error due to this being treated as
-    // a system include
+    // External clients shouldn't see the error due to this being treated as
+    // a system include and us using -Wno-system-headers
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wfloat-equal"
     return lhs == rhs;
